12d3920035d90b7088dcb92017cbffcffcf722fd
abdullahumuth <ultiminatea@gmail.com>
2024-08-05 22:21:07 +0200

    Update first_experiment.sh

--- first_experiment.sh
+++ first_experiment.sh
@@ -4,9 +4,9 @@
 #SBATCH --error=./data/outputs/log_file
 #SBATCH -p pgi-8-gpu
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
-#SBATCH --mem=32G
-#SBATCH --gres=gpu:a100:3
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=8G
+#SBATCH --gres=gpu:a100:1
 
 srun julia ./src/main.jl
 

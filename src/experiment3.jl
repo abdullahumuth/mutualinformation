d6921bb1ae5c22ac4482c44f231f0a6c4fe3062b
abdullahumuth <ultiminatea@gmail.com>
2024-08-12 17:55:31 +0200

    updated

--- src/experiment3.jl
+++ src/experiment3.jl
@@ -1,6 +1,6 @@
 include("./main.jl")
 
-name = "ER_improved_uniform_time_series_l20_batch128"
+name = "ER_improved_unique_uniform_time_series_l20_batch128"
 version = 1
 
 experiment3 = experiment(L, J, g, 0.0:0.05:1.0, 10000)

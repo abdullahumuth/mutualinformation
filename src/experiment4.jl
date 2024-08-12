0cad623643f56cf968b2546e2f2a54edcb7d0acd
abdullahumuth <ultiminatea@gmail.com>
2024-08-12 17:58:04 +0200

    4partition

--- src/experiment4.jl
+++ src/experiment4.jl
@@ -1,6 +1,6 @@
 include("./main.jl")
 
-name = "ER_improved_moment_of_truth_batch128_2partition"
+name = "ER_improved_moment_of_truth_batch128_4partition"
 version = 1
 experiment4 = experiment(L, J, g, 0.0:1.0:1.0, (2^x for x=9:16))
 experiment4(name, version; )

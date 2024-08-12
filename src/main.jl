b388987133b741616c3f569e0d3d8f4503f921a7
abdullahumuth <ultiminatea@gmail.com>
2024-08-12 16:15:55 +0200

    uniform time test.

--- src/main.jl
+++ src/main.jl
@@ -160,17 +160,17 @@ t = 0.1   # can be anything from collect(0:0.001:1)
 # test("test", 1, new = true)
 
 
-sample_experiment = experiment(L, J, g, 0.5:0.5:1.0, (2^x for x=9:16))
-sample_experiment("uniform_sample_convergence_l20_batch128", 1; )
+#sample_experiment = experiment(L, J, g, 0.5:0.5:1.0, (2^x for x=9:16))
+#sample_experiment("uniform_sample_convergence_l20_batch128", 1; )
 # sample_experiment("transfer_sample_convergence_large_batch128", 1; new = true)
 # sample_experiment("sample_convergence_large_batch256", 1; batch_size = 256)
 # sample_experiment("sample_convergence_large_batch512", 1; batch_size = 512)
 # sample_experiment("sample_convergence_large_batch1024", 1; batch_size = 1024)
 # sample_experiment("transfer_sample_convergence_large_batch1024", 1; batch_size = 1024, new = true)
 
-# time_evolve_experiment = experiment(20, J, g, 0.0:0.05:1.0, 10000)
-# 
-# time_evolve_experiment("time_evolve_convergence_large_batch256", 1, batch_size = 256)
+time_evolve_experiment = experiment(20, J, g, 0.0:0.05:1.0, 10000)
+
+time_evolve_experiment("uniform_time_evolve_l20_batch128", 1, batch_size = 128)
 # time_evolve_experiment("transfer_time_evolve_convergence_large_batch256", 1; new = true, batch_size = 256)
 
 

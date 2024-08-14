f98e5ef9a2cdd2b2288283f0429781a757fad5af
abdullahumuth <ultiminatea@gmail.com>
2024-08-14 14:56:21 +0200

    unique mag

--- src/experiment2.jl
+++ src/experiment2.jl
@@ -1,7 +1,7 @@
 include("./main.jl")
 
-name = "magnetization_moment_of_truth_2partition_l20_batch128"
+name = "unique_magnetization_moment_of_truth_2partition_l20_batch128"
 version = 1
 
-experiment2 = experiment(L, J, g, 0.5:0.45:0.95, (2^x for x=9:16))
+experiment2 = experiment(L, J, g, 0.5, (2^x for x=9:16))
 experiment2(name, version; )

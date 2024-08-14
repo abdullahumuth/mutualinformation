55e343992b1216f5a5ac74e9f8422ca61785c83b
abdullahumuth <ultiminatea@gmail.com>
2024-08-14 14:58:16 +0200

    shuffled guarantee check

--- src/experiment2.jl
+++ src/experiment2.jl
@@ -1,7 +1,7 @@
 include("./main.jl")
 
-name = "unique_magnetization_moment_of_truth_2partition_l20_batch128"
-version = 1
+name = "shuffled_magnetization_moment_of_truth_2partition_l20_batch128"
+version = 2
 
 experiment2 = experiment(L, J, g, 0.5, (2^x for x=9:16))
 experiment2(name, version; )

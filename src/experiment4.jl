bdde12a4a274748d134fdd7e7bf7ad2537e5575a
abdullahumuth <ultiminatea@gmail.com>
2024-08-16 16:04:24 +0200

    Update experiment4.jl

--- src/experiment4.jl
+++ src/experiment4.jl
@@ -1,7 +1,5 @@
 include("./main.jl")
 
-name = "magnetization_moment_of_truth_2partition_l20_batch128"
-version = 3
+name = "generate_samples_first"
 
-experiment2 = experiment(L, J, g, 0.5, (2^x for x=10:16))
-experiment2(name, version; uniform=true, fake=true, shuffle=false)
+generation_experiment(name, 2, 20, 100; generate = true, conditional = true)

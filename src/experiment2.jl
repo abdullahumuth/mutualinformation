d37939b0091313e50cc6090501f0960b923a94ce
abdullahumuth <ultiminatea@gmail.com>
2024-08-16 16:04:26 +0200

    Update experiment2.jl

--- src/experiment2.jl
+++ src/experiment2.jl
@@ -1,10 +1,10 @@
 include("./main.jl")
 
-name = "continuous_sanity_test"
-version = 2
+name = "better_conditionals"
+version = 1
 
-experiment2 = experiment(L, J, g, 0.5, (10000 for x=9:16))
-experiment2(name, version; gaussian_num = 32)
+experiment2 = experiment(L, J, g, 0.5, (2^x for x=9:16))
+experiment2(name, version;)
 
 
 

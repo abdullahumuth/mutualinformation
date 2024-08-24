3c07d8b042e5f6f4ef037c85348063b74f6ca38b
abdullahumuth <ultiminatea@gmail.com>
2024-08-24 02:41:40 +0200

    changed log dir

--- experiment4.sh
+++ experiment4.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=mut_inf_exp3
-#SBATCH --output=./data/outputs/output%j.%N.out
-#SBATCH --error=./data/outputs/error%j.%N.err
+#SBATCH --job-name=mut_inf_exp4
+#SBATCH --output=./data/outputs/_logs/output%j.%N.out
+#SBATCH --error=./data/outputs/_logs/error%j.%N.err
 #SBATCH -p pgi-8-gpu
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2

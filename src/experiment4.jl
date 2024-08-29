a1a99071ae3cf89cf7f4c7b455a256231b3985a7
abdullahumuth <ultiminatea@gmail.com>
2024-08-29 15:46:31 +0200

    fixed exp 4

--- src/experiment4.jl
+++ src/experiment4.jl
@@ -7,5 +7,5 @@ generation_experiment("continuous_production_generation", 1, 2, 2, L, 2^16; seed
 
 version = 1
 load_exp = experiment(L, J, g, 0.5, (2^x for x=7:16))
-load_exp("cont_loaded_data_test", version; load = "continuous_production_generation")
+load_exp("really_cont_loaded_data_test", version; load = "continuous_production_generation", discrete=false)
 

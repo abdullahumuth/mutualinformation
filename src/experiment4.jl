b504e3182dfe492af1dba16822605bf6e02c5e06
abdullahumuth <ultiminatea@gmail.com>
2024-08-29 15:46:56 +0200

    removed the again generation

--- src/experiment4.jl
+++ src/experiment4.jl
@@ -3,7 +3,7 @@ include("./main.jl")
 
 # generation_experiment("production_generation", 2, 1, 20, 2^16; discrete=false, get_data_from = "", conditional = true)
 
-generation_experiment("continuous_production_generation", 1, 2, 2, L, 2^16; seed=120, discrete=false, get_data_from = "", conditional = true)
+#generation_experiment("continuous_production_generation", 1, 2, 2, L, 2^16; seed=120, discrete=false, get_data_from = "", conditional = true)
 
 version = 1
 load_exp = experiment(L, J, g, 0.5, (2^x for x=7:16))

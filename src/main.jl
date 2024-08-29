702ae7e63b3ec09cfc607ac515cbe18451a5e0b7
abdullahumuth <ultiminatea@gmail.com>
2024-08-29 11:52:36 +0200

    Made the conventions a little easier to use & fixed issues

--- src/main.jl
+++ src/main.jl
@@ -166,33 +166,35 @@ function save_plots(a,b,c...)
     savefig(plot_models(a,b), "data/outputs/$(c[1])/plots/" * name_files(c...) * ".png")
 end
 
-function generator(name, input_dim=2, seq_len=20, other_seq_len=2, b_input_dim=1, num_samples=10000, seed=313; conditional=true, discrete=true)
+function generator(name, model_output_dim = 2, model_output_seq_len = 20, initially_generated_dim = 1, initially_generated_seq_len=2, num_samples=10000, seed=313; conditional=true, discrete=true)
     rng = Xoshiro(seed)
     if discrete
         if conditional
-            m = GeneralTransformer(a_input_dim = input_dim, b_input_dim = b_input_dim)
-            y = 2 .* rand(rng, (other_seq_len, num_samples)) .- 1
-            y = reshape(y, (1, size(y)...)) |> gpu
-            x = generate_samples(m, input_dim, seq_len, num_samples, y) |> gpu
+            m = GeneralTransformer(a_input_dim = model_output_dim, b_input_dim = initially_generated_dim)
+            y = (2 .* rand(rng, (initially_generated_dim, initially_generated_seq_len, num_samples)) .- 1) |> gpu
+            x = generate_samples(m, model_output_dim, model_output_seq_len, num_samples, y) |> gpu
             output = (x, y)
         else
-            m = GeneralTransformer(a_input_dim = input_dim)
-            x = generate_samples(m, input_dim, seq_len, num_samples, nothing) |> gpu
+            m = GeneralTransformer(a_input_dim = model_output_dim)
+            x = generate_samples(m, model_output_dim, model_output_seq_len, num_samples, nothing) |> gpu
             output = (x,)
         end
     else
         if conditional
-            m = GeneralTransformer(a_input_dim = input_dim, b_input_dim = b_input_dim, gaussian_num = 32)
-            x_proto = bitrand(rng, (other_seq_len, num_samples))
-            x = zeros((2, size(x_proto)...))
+            m = GeneralTransformer(a_input_dim = model_output_dim, b_input_dim = initially_generated_dim, gaussian_num = 32)
+            x_proto = bitrand(rng, (initially_generated_seq_len, num_samples))
+            if initially_generated_dim != 2
+                throw(ArgumentError("Only 2 initially generated dimensions are supported"))
+            end
+            x = zeros((initially_generated_dim, size(x_proto)...))
             x[1, :, :] .= x_proto
             x[2, :, :] .= 1 .- x_proto
             x = Int.(x) |> gpu
-            y = generate_samples(m, input_dim, seq_len, num_samples, x; discrete = false) |> gpu
-            output = (x, y)
+            y = generate_samples(m, model_output_dim, model_output_seq_len, num_samples, x; discrete = false) |> gpu
+            output = (y, x)
         else
-            m = GeneralTransformer(a_input_dim = input_dim, gaussian_num = 32)
-            y = generate_samples(m, input_dim, seq_len, num_samples, nothing; discrete = false) |> gpu
+            m = GeneralTransformer(a_input_dim = model_output_dim, gaussian_num = 32)
+            y = generate_samples(m, model_output_dim, model_output_seq_len, num_samples, nothing; discrete = false) |> gpu
             output = (y,)
         end
 
@@ -208,8 +210,13 @@ function generator(name, input_dim=2, seq_len=20, other_seq_len=2, b_input_dim=1
     h5open("data/inputs/$(name)/data/generated_" * name * ".h5", "w") do file
         g = create_group(file, "data")
         if length(output) > 1
-            g["x"] = cpu(output[1])
-            g["y"] = cpu(output[2])
+            if discrete
+                g["x"] = cpu(output[1])
+                g["y"] = cpu(output[2])
+            else
+                g["y"] = cpu(output[1])
+                g["x"] = cpu(output[2])
+            end
         else
             if discrete
                 g["x"] = cpu(output[1])
@@ -236,7 +243,7 @@ function load_generated_data(name, num_samples, discrete=true)
     else
         indices = randperm(Xoshiro(303), size(data["y"])[3])[1:num_samples]
         try
-            full_data = (data["x"][:,:,indices], data["y"][:,:,indices])
+            full_data = (data["y"][:,:,indices], data["x"][:,:,indices])
         catch e
             full_data = (data["y"][:,:,indices],)
         end
@@ -254,10 +261,10 @@ function evaluate(model, x, y; discrete = true)
     
 end
 
-function generation_experiment(name, input_dim = 2, output_dim=1, seq_len = 20, other_seq_len=2, num_samples = 10000; seed=313, get_data_from = "", discrete=true, conditional = true, kwargs...)
+function generation_experiment(name, model_output_dim = 2, model_output_seq_len = 20, initially_generated_dim = 1, initially_generated_seq_len=2, num_samples = 10000; seed=313, get_data_from = "", discrete=true, conditional = true, kwargs...)
     if get_data_from == ""
         get_data_from = name
-        m = generator(name, input_dim, seq_len, other_seq_len, output_dim, num_samples, seed; conditional = conditional, discrete = discrete)
+        m = generator(name, model_output_dim, model_output_seq_len, initially_generated_dim, initially_generated_seq_len, num_samples, seed; conditional = conditional, discrete = discrete)
     else
         m = load_generated_data(get_data_from, num_samples, discrete)
     end
@@ -267,10 +274,10 @@ function generation_experiment(name, input_dim = 2, output_dim=1, seq_len = 20, 
     gpu_model, gpu_data = m.model |> gpu, m.data |> gpu
 
     if conditional
-        a = evaluate(gpu_model, gpu_data...; kwargs...)
+        a = evaluate(gpu_model, gpu_data...; discrete = discrete, kwargs...)
         model_name = "conditional_entropy"
     else
-        a = evaluate(gpu_model, gpu_data[1], nothing; kwargs...)
+        a = evaluate(gpu_model, gpu_data[1], nothing; discrete = discrete, kwargs...)
         model_name = "entropy"
     end
     result = DataFrame(Symbol(model_name) => a)

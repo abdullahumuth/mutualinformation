8e598eeed0c8217461d6151d6972d886a2551d07
abdullahumuth <ultiminatea@gmail.com>
2024-09-04 19:38:00 +0200

    t = 1

--- src/experiment3.jl
+++ src/experiment3.jl
@@ -1,5 +1,5 @@
 include("./main.jl")
 
 version = 1
-load_exp = experiment(12, J, g, 0.1:0.4:0.5, (2^x for x=7:16))
+load_exp = experiment(12, J, g, 1.0, (2^x for x=7:16))
 load_exp("cont_l12_sample_test", version; gaussian_num = 32)

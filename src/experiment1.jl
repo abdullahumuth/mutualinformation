23949c3d29f2a99e0e469384f2db88af0beae8b1
abdullahumuth <ultiminatea@gmail.com>
2024-09-05 23:48:14 +0200

    New experiments

--- src/experiment1.jl
+++ src/experiment1.jl
@@ -1,14 +1,14 @@
 include("./main.jl")
 
-name = "tryy_new"
+name = "noisy_continuous"
 version = 1
 
 # experiment1 = experiment(L, J, g, 0.1, (2^x for x=5:6))
 # experiment1(name, version)
 
-data_gen_params = OrderedDict(:L => L, :J => J, :g => g, :t => 0.1, :num_samples=>(2^x for x=5:6))
-exp_mode = OrderedDict(:discrete => true)
-hyper_params = OrderedDict(:batch_size => 256)
+data_gen_params = OrderedDict(:L => [20, 12], :J => J, :g => g, :t => [0.1, 0.5, 1.0], :num_samples=>(2^x for x=5:16))
+exp_mode = OrderedDict(:discrete => false, :noise => [0.00001, 0.0001, 0.001, 0.01, 0.1])
+hyper_params = OrderedDict(:gaussian_num => 32)
 
 simple_experiment(name, version, data_gen_params; exp_modes_params = exp_mode, hyper_parameters = hyper_params)
 
--- src/experiment2.jl
+++ src/experiment2.jl
@@ -1,10 +1,14 @@
 include("./main.jl")
 
+name = "noise0.00001_discrete_sample"
 version = 1
 
-experiment2 = experiment(12:8:20, J, g, 0.1:0.8:0.9, (2^x for x=5:16))
-experiment2("noise0.001_discrete_sample", version; noise = 0.001)
-experiment2("noise0.01_discrete_sample", version; noise = 0.01)
-experiment2("noise0.1_discrete_sample", version; noise = 0.1)
+# experiment1 = experiment(L, J, g, 0.1, (2^x for x=5:6))
+# experiment1(name, version)
 
+data_gen_params = OrderedDict(:L => [20, 12], :J => J, :g => g, :t => [0.1, 0.5, 1.0], :num_samples=>(2^x for x=5:16))
+exp_mode = OrderedDict(:discrete => true, :noise => [0.00001, 0.0001])
+hyper_params = OrderedDict()
+
+simple_experiment(name, version, data_gen_params; exp_modes_params = exp_mode, hyper_parameters = hyper_params)
 

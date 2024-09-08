16bf27370f2b3e0cc2a450874f1341945d7ea359
abdullahumuth <ultiminatea@gmail.com>
2024-09-08 21:32:15 +0200

    new exp

--- src/experiment4.jl
+++ src/experiment4.jl
@@ -6,6 +6,12 @@ include("plot_from_json.jl")
 #generation_experiment("continuous_production_generation", 1, 2, 2, L, 2^16; seed=120, discrete=false, get_data_from = "", conditional = true)
 
 version = 1
-load_exp = experiment(L, J, g, 0.1, (2^x for x=7:16))
-load_exp("t01_cont_loaded_data_test", version; load = "continuous_production_generation", gaussian_num = 32)
+
+name = "discretely_generated_continuous_prediction"
+
+data_gen_params = OrderedDict(:L => L, :J => J, :g => g, :t => [0.1], :num_samples=>(2^x for x=9:16))
+exp_mode = OrderedDict(:load => "generated_production_generation")
+hyper_params = OrderedDict(:gaussian_num => 32)
+
+simple_experiment(name, version, data_gen_params; exp_modes_params = exp_mode, hyper_parameters = hyper_params)
 

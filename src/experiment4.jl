e352bb5ea758dd02ad664f9368e31e060a94bbea
abdullahumuth <ultiminatea@gmail.com>
2024-09-09 13:08:07 +0200

    Update experiment4.jl

--- src/experiment4.jl
+++ src/experiment4.jl
@@ -10,7 +10,7 @@ version = 1
 name = "discretely_generated_continuous_prediction"
 
 data_gen_params = OrderedDict(:L => L, :J => J, :g => g, :t => [0.1], :num_samples=>(2^x for x=9:16))
-exp_mode = OrderedDict(:load => "generated_production_generation")
+exp_mode = OrderedDict(:load => ["generated_production_generation"])
 hyper_params = OrderedDict(:gaussian_num => 32)
 
 simple_experiment(name, version, data_gen_params; exp_modes_params = exp_mode, hyper_parameters = hyper_params)

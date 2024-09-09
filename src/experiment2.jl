f34ff7a38e00ef93bca84994d499e6059f9d130e
abdullahumuth <ultiminatea@gmail.com>
2024-09-09 18:56:30 +0200

    new exp

--- src/experiment2.jl
+++ src/experiment2.jl
@@ -1,13 +1,13 @@
 include("./main.jl")
 include("plot_from_json.jl")
-name = "noise0.00001_discrete_sample"
+name = "noiseless_L12_discrete_sample"
 version = 1
 
 # experiment1 = experiment(L, J, g, 0.1, (2^x for x=5:6))
 # experiment1(name, version)
 
-data_gen_params = OrderedDict(:L => [20, 12], :J => J, :g => g, :t => [0.1, 0.5, 0.9], :num_samples=>(2^x for x=6:16))
-exp_mode = OrderedDict(:discrete => true, :noise => [0.00001, 0.0001])
+data_gen_params = OrderedDict(:L => [12], :J => J, :g => g, :t => [0.1, 0.5, 0.9], :num_samples=>(2^x for x=6:16))
+exp_mode = OrderedDict(:discrete => true, :noise => [0])
 hyper_params = OrderedDict()
 
 simple_experiment(name, version, data_gen_params; exp_modes_params = exp_mode, hyper_parameters = hyper_params)
